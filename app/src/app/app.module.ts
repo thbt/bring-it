8baa85bd0eb36490e5f7816ab924059fc4f636b2
Fotso Loïc <loic.fotso@etu.utc.fr>
2017-11-19 15:16:20 +0100

    create wish-list-page

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {WishlistPageModule} from "../pages/wishlist/wishlist.module";
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    WishlistPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
